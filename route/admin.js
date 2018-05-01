0b51d1109fd9e06e37e23dcc0dddb63275a0071e
Keller Me <kellerme@KellerdeMBP.lan>
2018-05-01 11:41:31 +0800

    重定向

--- route/admin.js
+++ route/admin.js
@@ -6,6 +6,15 @@ module.exports = function () {
     // router.get('/', function (req, res) {
     //     res.send('admin').end();
     // });
+    // 检查登录态
+    router.use((req, res, next) => {
+        if (!req.session['admin_id'] && req.url != '/login') {
+            // 重定向登录
+            res.redirect('/admin/login');
+        } else {
+            next();
+        }
+    });
     router.get('/login', function (req, res) {
         res.render('admin/login.ejs', {});
     });
