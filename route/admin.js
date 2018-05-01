9985e9b359dfa2fbb3c4d3341a8ed50ec7c9d401
Keller Me <kellerme@KellerdeMBP.lan>
2018-05-01 11:26:38 +0800

    login test

--- route/admin.js
+++ route/admin.js
@@ -3,8 +3,11 @@ const express = require('express');
 module.exports = function () {
     let router = express.Router();
 
-    router.get('/', function (req, res) {
-        res.send('admin').end();
+    // router.get('/', function (req, res) {
+    //     res.send('admin').end();
+    // });
+    router.get('/login', function (req, res) {
+        res.render('admin/login.ejs', {});
     });
 
     return router;
